bab3dce50572672c8a3cd61c6ce071fe77602c7e
zcs <625834120@qq.com>
2016-08-09 00:59:31 +0800

    switch的值绑定
    如何就会进行了绑定

--- Int8_Int16.playground/Pages/switch.xcplaygroundpage/Contents.swift
+++ Int8_Int16.playground/Pages/switch.xcplaygroundpage/Contents.swift
@@ -22,3 +22,77 @@ default:
     
 }
 
+//switch 区间匹配 ...从这个也可以看出来，当count 与switch匹配时，就会跳出switch, 其中case当中的代码块不需要加括号
+
+ let count = 3
+
+switch count{
+
+case 0...3:
+    print("count = 0...3")
+    print("count = 0...3")
+    print("count = 0...3")
+case 3...6:
+    print("count = 3...6")
+    print("count = 3...6")
+    print("count = 3...6")
+default:
+    print("\(someSwitch)是辅音字母")
+
+}
+
+
+//switch 与元祖tuple 进行匹配，对于任意值时，在swify中用 "_",表示
+
+let 坐标 = (1,3)
+
+switch 坐标{
+    
+case (0,0): "坐标原点"
+ 
+case(_,0): "在x轴上"
+case(0,_): "在y轴上"
+case(-2...2,-2...3): "在指定的区域内"
+    
+default: "非指定区域"
+    
+}
+
+
+// switch 值绑定 ,其中x,和y都是临时变量，即值绑定变量职能在你绑定的case中用，其他地方不能使用
+let point = (1,1);
+
+switch point{
+    
+case(let x,0): "x轴上的值是\(x)"
+case(0,let y): "y轴上的值是\(y)"
+    
+case(let x,let y): "x轴上的值是\(x),y轴上的值是\(y)"
+    
+//default: "other"  上面一个case代替了case
+    
+}
+
+
+//switch 值绑定，用where进行筛选,where相当于if自己的理解，但此处不能用if
+
+let point2 = (0,2)
+
+switch point2 {
+
+case let (x,y) where x>0 && y>0 : "\(x),\(y)在第一象限"
+case let (x,y) where x<0 && y>0 : "\(x),\(y)在第二象限"
+case let (x,y) where x<0 && y<0 : "\(x),\(y)在第三象限"
+case let (x,y) where x>0 && y<0 : "\(x),\(y)在第四象限"
+   
+case(let x,let y): "x轴上的值是\(x),y轴上的值是\(y)"
+//default: "在轴上" 上面一个case代替了case
+}
+
+
+// switch 中的关键字 continue
+
+
+
+
+
